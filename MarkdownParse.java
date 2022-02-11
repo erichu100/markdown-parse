fee9a6d278180826f07a0a5a5fc849b73dfbea20
Cat Meow <erichu100@outlook.com>
2022-02-10 16:51:47 -0800

    pre-merge commit

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,11 +4,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+//MewMewMeowMewasdfasd
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
+        //cat
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             boolean validLink = true;
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -9,6 +9,7 @@ import org.junit.*;
 
 //javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
 //java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+//catcat
 public class MarkdownParseTest {
     @Test
     public void addition() {
