d76991b11df79184e5a7365b355c6093704cdaac
Eric Hu <erichu100@outlook.com>
2022-02-10 17:10:08 -0800

    Added linux commands to MarkdownParseTest.java comments.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 import org.junit.*;
 
+//Windows:
 //javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
 //java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+//Linux:
+//javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+//java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
     @Test
     public void addition() {
